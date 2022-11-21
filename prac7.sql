2d9f2b8194bb3ee055757540cb49e9aa655cf370
joren <joren.liegeois@gmail.com>
2022-11-21 09:55:03 +0100

    prac7 bijna klaar

--- prac7.sql
+++ prac7.sql
@@ -38,7 +38,81 @@ ORDER BY right(naam, 1) DESC
 -- 4
 
 SELECT naam, (SELECT count(spelersnr)
-FROM tennis3.wedstrijden w 
-WHERE w.spelersnr = s.spelersnr) as aantal
-FROM tennis3.spelers S
+FROM tennis3.boetes b 
+WHERE b.spelersnr = s.spelersnr) as aantal
+FROM tennis3.spelers s
 ORDER BY aantal DESC, naam ASC
+-- oef 3
+-- 1
+
+SELECT * 
+FROM tennis3.boetes
+ORDER BY bedrag DESC
+LIMIT 3
+-- 2
+
+SELECT *
+FROM tennis3.spelers
+WHERE plaats = (
+    SELECT plaats
+    FROM tennis3.spelers
+    WHERE naam = "Bischoff")
+ORDER BY jaartoe DESC
+LIMIT 3
+-- 3
+
+SELECT naam, geb_datum, (
+    SELECT count(spelersnr)
+    FROM tennis3.boetes b
+    WHERE b.spelersnr = s.spelersnr) as aantal
+FROM tennis3.spelers s
+ORDER BY aantal DESC, geb_datum ASC
+LIMIT 3
+-- oef 4
+-- 1
+
+SELECT jaartoe
+FROM tennis3.spelers
+GROUP BY jaartoe
+-- 2
+
+SELECT jaartoe, count(*) as aantal
+FROM tennis3.spelers
+GROUP BY jaartoe
+-- 3
+
+SELECT count(teamnr) as Wedstrijden, sum(GEWONNEN) as aantal_gewonnen_sets
+FROM tennis3.wedstrijden
+WHERE teamnr = (
+    SELECT teamnr
+    FROM tennis3.teams
+    WHERE divisie = "ere") /* werkt wel maar gebruikt geen group by */
+-- 4
+
+SELECT spelersnr, sum(bedrag) as totaal_bedrag, (
+    SELECT NAAM
+    FROM tennis3.spelers S
+    where S.spelersnr = b.spelersnr 
+) as naam
+FROM tennis3.boetes b
+GROUP BY spelersnr
+-- 5
+
+SELECT teamnr, sum(GEWONNEN) as aantal_gewonnen_sets, (
+    SELECT divisie
+    FROM tennis3.teams t
+    WHERE t.teamnr = w.teamnr
+) as Divisie
+FROM tennis3.wedstrijden w
+GROUP BY teamnr
+-- 6
+
+SELECT spelersnr, (
+    SELECT count(spelersnr)
+    FROM tennis3.BOETES b
+    WHERE b.spelersnr = w.spelersnr
+) as boetes
+FROM tennis3.wedstrijden w
+GROUP BY spelersnr
+-- 7
+
