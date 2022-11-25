4458f3e6c9545098c6cd87d3697125635bf76542
joren <joren.liegeois@gmail.com>
2022-11-25 14:22:33 +0100

    some style changes

--- prac7.sql
+++ prac7.sql
@@ -8,49 +8,48 @@ A) W2 en Null
 B) W2 W3, W2 Null, Null Null
 */ 
 -- 2
-
 SELECT DISTINCT plaats
 FROM tennis3.spelers
--- 3
 
+-- 3
 SELECT DISTINCT bedrag, month(datum)
 FROM tennis3.boetes
---4
 
+--4
 SELECT DISTINCT functie
 FROM tennis3.bestuursleden
+
 -- oef 2
 -- 1
-
 SELECT * 
 FROM tennis3.spelers
 ORDER BY naam DESC
--- 2
 
+-- 2
 SELECT *
 FROM tennis3.spelers
 ORDER BY jaartoe, plaats, naam DESC
--- 3
 
+-- 3
 SELECT naam, voorletters, spelersnr
 FROM tennis3.spelers
 ORDER BY right(naam, 1) DESC
--- 4
 
+-- 4
 SELECT naam, (SELECT count(spelersnr)
 FROM tennis3.boetes b 
 WHERE b.spelersnr = s.spelersnr) as aantal
 FROM tennis3.spelers s
 ORDER BY aantal DESC, naam ASC
+
 -- oef 3
 -- 1
-
 SELECT * 
 FROM tennis3.boetes
 ORDER BY bedrag DESC
 LIMIT 3
--- 2
 
+-- 2
 SELECT *
 FROM tennis3.spelers
 WHERE plaats = (
@@ -59,8 +58,8 @@ WHERE plaats = (
     WHERE naam = "Bischoff")
 ORDER BY jaartoe DESC
 LIMIT 3
--- 3
 
+-- 3
 SELECT naam, geb_datum, (
     SELECT count(spelersnr)
     FROM tennis3.boetes b
@@ -68,27 +67,27 @@ SELECT naam, geb_datum, (
 FROM tennis3.spelers s
 ORDER BY aantal DESC, geb_datum ASC
 LIMIT 3
+
 -- oef 4
 -- 1
-
 SELECT jaartoe
 FROM tennis3.spelers
 GROUP BY jaartoe
--- 2
 
+-- 2
 SELECT jaartoe, count(*) as aantal
 FROM tennis3.spelers
 GROUP BY jaartoe
--- 3
 
+-- 3
 SELECT count(teamnr) as Wedstrijden, sum(GEWONNEN) as aantal_gewonnen_sets
 FROM tennis3.wedstrijden
 WHERE teamnr = (
     SELECT teamnr
     FROM tennis3.teams
     WHERE divisie = "ere") /* werkt wel maar gebruikt geen group by */
--- 4
 
+-- 4
 SELECT spelersnr, sum(bedrag) as totaal_bedrag, (
     SELECT NAAM
     FROM tennis3.spelers S
@@ -96,8 +95,8 @@ SELECT spelersnr, sum(bedrag) as totaal_bedrag, (
 ) as naam
 FROM tennis3.boetes b
 GROUP BY spelersnr
--- 5
 
+-- 5
 SELECT teamnr, sum(GEWONNEN) as aantal_gewonnen_sets, (
     SELECT divisie
     FROM tennis3.teams t
@@ -105,8 +104,8 @@ SELECT teamnr, sum(GEWONNEN) as aantal_gewonnen_sets, (
 ) as Divisie
 FROM tennis3.wedstrijden w
 GROUP BY teamnr
--- 6
 
+-- 6
 SELECT spelersnr, (
     SELECT count(spelersnr)
     FROM tennis3.BOETES b
@@ -114,5 +113,6 @@ SELECT spelersnr, (
 ) as boetes
 FROM tennis3.wedstrijden w
 GROUP BY spelersnr
+
 -- 7
 
