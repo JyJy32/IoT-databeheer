5079d2093c7d0f52cbba836138778080ff7a4306
joren <joren.liegeois@gmail.com>
2022-11-25 10:13:46 +0100

    deel van prac 8

---
+++ prac8.bash
@@ -0,0 +1,26 @@
+# oef 2
+# 1
+mosquitto -help
+# 2
+mosquitto
+# 3
+mosquitto_pub --help
+or
+man mosquitto_pub
+# 4
+mosquitto_pub --help
+or
+man mosquitto_pub
+# 5
+
+-- 11
+mosquitto_sub -h localhost -p 1883 -t "/AP/+/+/temperature" -i mosquitto_sub_ap -v > mqttlog
+
+# oef 2
+# 1
+cp /usr/share/doc/mosquitto/examples/mosquitto.conf /home/<name>
+#in security uncomment prefix en typ ap- er achter
+# 2
+mosquitto_sub -h localhost -p 1883 -t /AP/liegeois/joren
+# 3
+mosquitto_sub -h localhost -p 1883 -t /AP/liegeois/joren -i ap-joren
